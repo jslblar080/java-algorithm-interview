adfd6e04abb7779b11fbfb922d23fd3499bd3422
jslblar080 <jslblar080@gmail.com>
2024-04-18 01:00:54 +0900

    better solution with toByteArray() method

--- src/ch06/ValidPalindrome.kt
+++ src/ch06/ValidPalindrome.kt
@@ -3,11 +3,12 @@ package ch06
 class Solution {
     fun isPalindrome(s: String): Boolean {
         val str = s.replace("[^a-zA-Z0-9]".toRegex(), "").toLowerCase()
-        val len = str.length
+        val bytes = str.toByteArray()
+        val len = bytes.size
         val mid = len / 2
 
         for (i in 0 until mid) {
-            if (str[i] != str[len - i - 1]) {
+            if (bytes[i] != bytes[len - i - 1]) {
                 return false
             }
         }
