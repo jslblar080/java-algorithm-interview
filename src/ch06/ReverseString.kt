937c236027922e1d09ee0854ea1ace1beb5d004d
jslblar080 <jslblar080@gmail.com>
2024-04-20 17:10:42 +0900

    final solution with initializing CharArray with size

--- src/ch06/ReverseString.kt
+++ src/ch06/ReverseString.kt
@@ -3,7 +3,7 @@ package ch06
 class Solution2 {
     fun reverseString(s: CharArray): Unit {
         val len = s.size
-        val s2 = CharArray(len)
+        val s2 = CharArray(len) // Initialize CharArray with size
 
         for (i in len-1 downTo 0) {
             s2[len - 1 - i] = s[i]
