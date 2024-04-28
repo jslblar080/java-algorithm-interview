c5b59fa6c9a3cfbdc805c846581d3dccf8873592
jslblar080 <jslblar080@gmail.com>
2024-04-29 03:05:06 +0900

    solution with ValidPalindrome in Java

---
+++ src/ch06/ValidPalindrome.java
@@ -0,0 +1,27 @@
+package ch06;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama")); // true
+        System.out.println(isPalindrome("race a car")); // false
+        System.out.println(isPalindrome(" ")); // true
+    }
+
+    public static boolean isPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            while(i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+
+            while(i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
