34fde4fe0af2cf28eb1b60f97f0c64da0117c605
jslblar080 <jslblar080@gmail.com>
2024-05-20 15:04:14 +0900

    add indentation

--- src/ch06/ReverseString.java
+++ src/ch06/ReverseString.java
@@ -1,6 +1,7 @@
 package ch06;
 
 public class ReverseString {
+
     public static void main(String[] args) {
         reverseString(new char[]{'h', 'e', 'l', 'l', 'o'});
         reverseString(new char[]{'H', 'a', 'n', 'n', 'a', 'h'});
--- src/ch06/ValidPalindrome.java
+++ src/ch06/ValidPalindrome.java
@@ -1,6 +1,7 @@
 package ch06;
 
 public class ValidPalindrome {
+
     public static void main(String[] args) {
         System.out.println(isPalindrome("A man, a plan, a canal: Panama")); // true
         System.out.println(isPalindrome("race a car")); // false
@@ -9,11 +10,11 @@ public class ValidPalindrome {
 
     public static boolean isPalindrome(String s) {
         for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
-            while(i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
                 i++;
             }
 
-            while(i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
                 j--;
             }
 
